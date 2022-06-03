14e4c96c66893bc76b941cb9889ac0375be532b4
Sabjit Singh <75779054+SabjitSingh@users.noreply.github.com>
2022-06-03 10:36:29 +0530

    Add files via upload

---
+++ src/Math_basics_problems/seive_of_Eratosthene.java
@@ -0,0 +1,41 @@
+package Math_basics_problems;
+import java.util.*;
+public class seive_of_Eratosthene {
+    /*package whatever //do not write package name here */
+
+        static void sieve(int n)
+        {
+            if(n <= 1)
+                return;
+
+            boolean isPrime[] = new boolean[n+1];
+
+            Arrays.fill(isPrime, true);
+
+            for(int i=2; i*i <= n; i++)
+            {
+                if(isPrime[i])
+                {
+                    for(int j = 2*i; j <= n; j = j+i)
+                    {
+                        isPrime[j] = false;
+                    }
+                }
+            }
+
+            for(int i = 2; i<=n; i++)
+            {
+                if(isPrime[i])
+                    System.out.print(i+" ");
+            }
+        }
+
+        public static void main (String[] args) {
+
+            int n = new Scanner(System.in).nextInt();
+
+            sieve(n);
+
+        }
+    }
+
